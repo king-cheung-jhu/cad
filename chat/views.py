57e76046ee35ed8ffd046906ffccc7567a1e3ef1
Taylor Brown <taylor7brown@gmail.com>
2019-05-12 22:26:40 -0400

    fixed views.py

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,12 @@
 # chat/views.py
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.safestring import mark_safe
 import json
 from django.views.generic import CreateView, TemplateView, View, FormView
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from templates.chat.forms import UserForm
 
 
 class RoomView(TemplateView):
@@ -16,8 +19,10 @@ class RoomView(TemplateView):
         context = super(RoomView, self).dispatch(request, *args, **kwargs)
         return context
 
+
 def index(request):
     return render(request, 'chat/index.html', {'username': mark_safe(json.dumps(request.user.username))})
+
 
 @login_required
 def room(request, room_name):
@@ -25,3 +30,21 @@ def room(request, room_name):
         'room_name_json': mark_safe(json.dumps(room_name)),
         'username': mark_safe(json.dumps(request.user.username))
     })
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'templates/chat/register.html'
+
+    def get(self, request):
+        from = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            user.save()
--- game/views.py
+++ game/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
 from django.views.generic import CreateView, TemplateView, View, FormView
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
+
 
 class GameView(TemplateView):
     template_name = 'room.html'
@@ -15,8 +20,10 @@ class GameView(TemplateView):
         context = super(GameView, self).dispatch(request, *args, **kwargs)
         return context
 
+
 def index(request):
     return render(request, 'chat/index.html', {})
+
 
 @login_required
 def room(request, room_name):
@@ -24,3 +31,33 @@ def room(request, room_name):
         'room_name_json': mark_safe(json.dumps(room_name)),
         'username': mark_safe(json.dumps(request.user.username))
     })
+
+
+class userFormView(View):
+    form_class = UserForm
+    template_name = 'templates/chat/register.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            # normalized data
+            username = form.cleaned_data['username']
+            user.save()
+
+            user = authenticate(username=username)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('templates:chat:index')
+
+        return render(request, self.template_name, {'form': form})
